f84b6d8b5e9d1a4995469a485d58a2f4f8514f7b
tar-pal <tpalamarchuk@gmail.com>
2024-10-30 15:40:02 +0200

    Cleaning

--- prepare.py
+++ prepare.py
@@ -1,15 +1,5 @@
-import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import classification_report
-import joblib
 
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -17,18 +7,15 @@ from sklearn.preprocessing import StandardScaler
 # Завантаження даних
 file_path = 'loan_data.csv'  # Заміни на шлях до твого файлу
 loan_data = pd.read_csv(file_path)
-#
-# # Використання OneHotEncoder для кодування змінної `purpose`
-# encoder = OneHotEncoder(sparse_output=False, drop='first')
-# purpose_encoded = encoder.fit_transform(loan_data[['purpose']])
-# purpose_encoded_df = pd.DataFrame(purpose_encoded, columns=encoder.get_feature_names_out(['purpose']))
 
+# Формуємо датафрейм
 data_frame = pd.DataFrame(loan_data)
 
+# Трансформуємо стовбчик purpose
 data_frame['purpose'] = data_frame['purpose'].astype('category').cat.codes
 
-# loan_data = pd.concat([loan_data.drop(columns=['purpose']), purpose_encoded_df], axis=1)
 loan_data = data_frame
+
 # Аналіз даних
 print("Опис даних:")
 print(loan_data.info())  # Основна інформація про датасет
@@ -48,9 +35,7 @@ heatmap.set_title('Correlation Matrix of Loan Data', fontsize=16)
 plt.show()
 
 # Підготовка даних
-# One-hot encoding для категоріальної змінної 'purpose'
 encoded_data =  loan_data
-# encoded_data =  pd.get_dummies(loan_data, columns=['purpose'], drop_first=True)
 
 # Нормалізація числових змінних
 scaler = StandardScaler()
